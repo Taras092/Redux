2297e8efcf64f526c6312e1c19ae0f60d2ccdbc3
taras <taraslychynets@icloud.com>
2022-02-01 14:14:41 +0200

    add state and change context in App

--- 1ReactContext/Context-ThemedButtons/src/App.jsx
+++ 1ReactContext/Context-ThemedButtons/src/App.jsx
@@ -1,15 +1,30 @@
-import React from "react";
-import ThemedButton from './ThemedButton'
+import React from 'react';
+import ThemedButton from './ThemedButton';
+import { themes, ThemeContext } from './themes-context';
 
 class App extends React.Component {
-render() {
-  return (
-    <div>
-      <ThemedButton>Dynamic Theme</ThemedButton>
-      <ThemedButton>Default Theme</ThemedButton>
-    </div>
-  );
-}
+  state = {
+    theme: themes.light,
+  };
+
+  toggleTheme = () => {
+    const newTheme = this.state.theme === themes.light ? themes.dark : themes.light;
+    
+    this.setState({
+      theme: newTheme,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <ThemeContext.Provider value={this.state.theme}>
+          <ThemedButton onClick={this.toggleTheme}>Dynamic Theme</ThemedButton>
+        </ThemeContext.Provider>
+        <ThemedButton onClick={this.toggleTheme}>Default Theme</ThemedButton>
+      </div>
+    );
+  }
 }
 
 export default App;
