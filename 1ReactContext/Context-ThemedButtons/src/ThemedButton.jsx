7c45260d375b1b6e69cbebd7f104a637d7eb271f
taras <taraslychynets@icloud.com>
2022-02-01 14:42:40 +0200

    render button with specific theme and work with this.context

--- 1ReactContext/Context-ThemedButtons/src/ThemedButton.jsx
+++ 1ReactContext/Context-ThemedButtons/src/ThemedButton.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { ThemeContext } from "themes-context";
+import { ThemeContext } from "./themes-context";
 
 class ThemedButton extends React.Component {
   render() {
     this.context;
-    return <button {...this.props} class="btn" />;
+    return (
+      <button
+        {...this.props}
+        style={{
+          backgroundColor: this.context.background,
+          color: this.context.fontColor,
+        }}
+        className="btn"
+      />
+    );
   }
 }
 
