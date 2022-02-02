319b9d4f52feb2c4452e7f2f05bac74126303fc9
taras <taraslychynets@icloud.com>
2022-02-02 16:44:59 +0200

    reducer function

---
+++ 2IntroToRedux/Redux-action-with-data/src/users.reducer.js
@@ -0,0 +1,18 @@
+import  { ADD_USER, DELETE_USER, initialState } from './store';
+
+export const createReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_USER:
+      return {
+        ...state,
+        usersList: state.usersList.concat([{ id: 76, name: 'Sarah' }]),
+      };
+    case DELETE_USER:
+      return {
+        ...state,
+        usersList: [],
+      };
+    default:
+      return state;
+  }
+};
