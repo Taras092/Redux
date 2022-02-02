b1426aaacc1c79a0618ad779bda7cb1fdd5da358
taras <taraslychynets@icloud.com>
2022-02-02 18:09:05 +0200

    actions function

--- 2IntroToRedux/Redux-action-with-data/src/users.actions.js
+++ 2IntroToRedux/Redux-action-with-data/src/users.actions.js
@@ -1,4 +1,21 @@
-import { ADD_USER, DELETE_USER, initialState } from './store';
+import { initialState } from './store';
+
+export const ADD_USER = 'ADD__USER';
+export const DELETE_USER = 'DELETE_USER';
+
+export const addUser = user => {
+  return {
+    type: ADD_USER,
+    payload: { user },
+  };
+};
+
+export const deleteUser = id => {
+  return {
+    type: DELETE_USER,
+    payload: { id },
+  };
+};
 
 export const actionsCreators = (state = initialState, action) => {
   switch (action.type) {
