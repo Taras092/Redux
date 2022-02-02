baaef4a13146a471cc6541aa16e2fbc8abfbc85f
taras <taraslychynets@icloud.com>
2022-02-02 17:01:08 +0200

    init

--- 2IntroToRedux/Redux-action-with-data/src/index.js
+++ 2IntroToRedux/Redux-action-with-data/src/index.js
@@ -1,21 +1,17 @@
-import store, { addUser, deleteUser } from './store';
+import store, { addUser, deleteUser, store1 } from './store';
+
+store1.subscribe(() => {
+  console.log(store1.getState());
+});
+
+store1.dispatch(addUser({ id: 1, name: 'Bob', age: 21 }));
+// store.dispatch(deleteUser());
+store1.dispatch(deleteUser(1));
 
 store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch(addUser({ id: 1, name: 'Bob', age: 21 }));
+store.dispatch(addUser());
 // store.dispatch(deleteUser());
-store.dispatch(deleteUser(1));
-// const users = [
-//   { id: 1, name: 'Bob', age: 21 },
-//   { id: 2, name: 'Sam', age: 45 },
-//   { id: 3, name: 'Con', age: 22 },
-//   { id: 4, name: 'Man', age: 55 },
-//   { id: 5, name: 'Boy', age: 55 },
-//   { id: 6, name: 'Her', age: 31 },
-// ];
-
-// const usersReducer = users.map(user => store.dispatch(addUser(user)));
-
-// console.log(usersReducer);
+store.dispatch(deleteUser());
