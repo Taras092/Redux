bd5a8af98f350e3f3e8fc57efb0780d4d95126a6
taras <taraslychynets@icloud.com>
2022-02-02 16:56:44 +0200

    init

--- 2IntroToRedux/Redux-action-with-data/src/index.js
+++ 2IntroToRedux/Redux-action-with-data/src/index.js
@@ -4,18 +4,18 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch(addUser());
-store.dispatch(deleteUser());
+store.dispatch(addUser({ id: 1, name: 'Bob', age: 21 }));
+// store.dispatch(deleteUser());
+store.dispatch(deleteUser(1));
+// const users = [
+//   { id: 1, name: 'Bob', age: 21 },
+//   { id: 2, name: 'Sam', age: 45 },
+//   { id: 3, name: 'Con', age: 22 },
+//   { id: 4, name: 'Man', age: 55 },
+//   { id: 5, name: 'Boy', age: 55 },
+//   { id: 6, name: 'Her', age: 31 },
+// ];
 
-const users = [
-  { id: 1, name: 'Bob', age: 21 },
-  { id: 2, name: 'Sam', age: 45 },
-  { id: 3, name: 'Con', age: 22 },
-  { id: 4, name: 'Man', age: 55 },
-  { id: 5, name: 'Boy', age: 55 },
-  { id: 6, name: 'Her', age: 31 },
-];
+// const usersReducer = users.map(user => store.dispatch(addUser(user)));
 
-const usersReducer = users.map(user => store.dispatch(addUser(user)));
-
-console.log(usersReducer);
+// console.log(usersReducer);
