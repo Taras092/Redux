aea030594d47837f14a060f1c3429d41860e4cfa
taras <taraslychynets@icloud.com>
2022-02-02 18:08:40 +0200

    init

--- 2IntroToRedux/Redux-action-with-data/src/index.js
+++ 2IntroToRedux/Redux-action-with-data/src/index.js
@@ -1,16 +1,17 @@
-import store, { addUser, deleteUser, store1 } from './store';
-
-// store1.subscribe(() => {
-//   console.log(store1.getState());
-// });
-
-// store1.dispatch(addUser({ id: 1, name: 'Bob', age: 21 }));
-// store1.dispatch(deleteUser(1));
+import store from './store';
+// import { addUser, deleteUser } from './users.reducer';
+import { addUser, deleteUser } from './users.actions';
 
 store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch(addUser());
+store.dispatch(addUser({ id: 1, name: 'Bob', age: 21 }));
+store.dispatch(deleteUser(1));
+
+// store.subscribe(() => {
+//   console.log(store.getState());
+// });
+
+// store.dispatch(addUser());
 // store.dispatch(deleteUser());
-store.dispatch(deleteUser());
