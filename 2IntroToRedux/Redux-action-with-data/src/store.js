92cf6dfdae5cd98bef4900f836da42c89711d029
taras <taraslychynets@icloud.com>
2022-02-03 12:45:29 +0200

    init

--- 2IntroToRedux/Redux-action-with-data/src/store.js
+++ 2IntroToRedux/Redux-action-with-data/src/store.js
@@ -1,12 +1,6 @@
 import { createStore } from 'redux';
-<<<<<<< HEAD
-import { userReducer } from './users.reducer';
-
-const store = createStore(userReducer);
-=======
 import { usersReducer } from "./users.reducer";
 
 const store = createStore(usersReducer);
->>>>>>> af2a2c5b720796c809a2860d6af394a33bb6bd57
 
 export default store;
--- 2IntroToRedux/Redux-action-with-data/src/users.actions.js
+++ 2IntroToRedux/Redux-action-with-data/src/users.actions.js
@@ -14,7 +14,3 @@ export const deleteUser = userId => {
     payload: { userId },
   };
 };
-<<<<<<< HEAD
-
-=======
->>>>>>> af2a2c5b720796c809a2860d6af394a33bb6bd57
