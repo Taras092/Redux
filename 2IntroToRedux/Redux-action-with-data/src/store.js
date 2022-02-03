4e3d0b60c2fb959b9abf7141b65f499da1b2bb7f
taras <taraslychynets@icloud.com>
2022-02-03 10:05:39 +0200

    create state in store

--- 2IntroToRedux/Redux-action-with-data/src/store.js
+++ 2IntroToRedux/Redux-action-with-data/src/store.js
@@ -2,23 +2,6 @@ import { createStore } from 'redux';
 import { createReducer } from './users.reducer';
 import { actionsCreators } from './users.actions';
 
-// export const ADD_USER = 'ADD__USER';
-// export const DELETE_USER = 'DELETE_USER';
-
-// export const addUser = (user) => {
-//   return {
-//     type: ADD_USER,
-//     payload: user,
-//   }
-// }
-
-// export const deleteUser = (id) => {
-//   return {
-//     type: DELETE_USER,
-//     payload: {id},
-//   };
-// }
-
 export const initialState = {
   usersList: [],
 };
