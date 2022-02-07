2af620576b06c0a93982e8f94661221e8c400658
taras <taraslychynets@icloud.com>
2022-02-07 19:10:43 +0200

    test file

---
+++ Lessons3/task1/src/index.js
@@ -0,0 +1,18 @@
+import store from './store';
+import { addUser, deleteUser } from './users.actions';
+import { increment, decrement, reset } from './counter.actions';
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+
+store.dispatch(addUser({ id: 1, name: 'Bob', age: 21 }));
+store.dispatch(addUser({ id: 2, name: 'Sam', age: 45 }));
+store.dispatch(addUser({ id: 3, name: 'Con', age: 22 }));
+store.dispatch(addUser({ id: 5, name: 'Boy', age: 55 }));
+store.dispatch(deleteUser(2));
