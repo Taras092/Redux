651a4734bb0899ce2375b62cdd172e7f366dd28c
taras <taraslychynets@icloud.com>
2022-02-07 23:32:40 +0200

    store

--- Lessons3/task3/src/store.js
+++ Lessons3/task3/src/store.js
@@ -3,24 +3,22 @@ import cartReducer from './cart.reducer';
 import userReducer from './user.reducer';
 import languageReducer from './language.reducer';
 
-export const initialState = {
-  language: 'en',
-  user: null,
-  cart: {
-    products: [],
-  },
-};
-
-// const appReducer = combineReducers({
-//   language: languageReducer,
-//   user: userReducer,
+// export const initialState = {
+//   language: 'en',
+//   user: null,
 //   cart: {
-//     cartReducer,
-//   }
-// });
+//     products: [],
+//   },
+// };
+
+const appReducer = combineReducers({
+  language: languageReducer,
+  user: userReducer,
+  cart: cartReducer,
+});
 
 const store = createStore(
-  userReducer,
+  appReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
