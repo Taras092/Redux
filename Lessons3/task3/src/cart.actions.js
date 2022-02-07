32fc063ca8a46af52c0f0f88d944a8ff2526e69b
taras <taraslychynets@icloud.com>
2022-02-07 23:38:24 +0200

    cart store

--- Lessons3/task3/src/cart.actions.js
+++ Lessons3/task3/src/cart.actions.js
@@ -1,14 +1,14 @@
 export const ADD_CART = 'CART/ADD_CART';
 export const DELETE_CART = 'CART/DELETE_CART';
 
-export const addCart = product => {
+export const addProduct = product => {
   return {
     type: ADD_CART,
     payload: { product },
   };
 }
 
-export const deleteCart = productId => {
+export const removeProduct = productId => {
   return {
     type: DELETE_CART,
     payload: { productId },
