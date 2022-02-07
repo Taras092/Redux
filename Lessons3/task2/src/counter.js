490168a4385eb74110f4a32d6c3ee868adfcc3fe
taras <taraslychynets@icloud.com>
2022-02-07 19:50:07 +0200

    counter

---
+++ Lessons3/task2/src/counter.js
@@ -0,0 +1,38 @@
+import { createStore, combineReducers } from 'redux';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+
+export const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+export const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const appReducer = combineReducers({
+  counter: counterReducer,
+});
+
+const store = createStore(
+  appReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+export default store;
