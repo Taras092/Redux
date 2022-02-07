ad5396fae1f1d4f2d1cf31823816cd2d2edf7360
taras <taraslychynets@icloud.com>
2022-02-07 20:32:16 +0200

    counter

--- Lessons3/task2/src/counter.js
+++ Lessons3/task2/src/counter.js
@@ -15,7 +15,7 @@ export const decrement = () => {
   };
 };
 
-export const counter = (state = 0, action) => {
+const counter = (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
       return state + 1;
