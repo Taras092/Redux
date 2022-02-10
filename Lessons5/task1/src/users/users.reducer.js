96e108a7b1795b245687ff1fb1cd2e0d2dfbc682
taras <taraslychynets@icloud.com>
2022-02-10 13:45:20 +0200

    store with selectors

---
+++ Lessons5/task1/src/users/users.reducer.js
@@ -0,0 +1,31 @@
+import { GO_PREV, GO_NEXT } from './users.actions';
+import users from '../users';
+
+const initialState = {
+  users: { usersList: [...users], currentPage: 1 },
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GO_PREV:
+      return {
+        ...state,
+        users: {
+          ...state.users,
+          currentPage: state.users.currentPage - 1,
+        },
+      };
+    case GO_NEXT:
+      return {
+        ...state,
+        users: {
+          ...state.users,
+          currentPage: state.users.currentPage + 1,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
