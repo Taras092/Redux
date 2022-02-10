ac01a276d592c92ec0ade167ccf103fcfc27657e
taras <taraslychynets@icloud.com>
2022-02-10 13:44:04 +0200

    react component

---
+++ Lessons5/task1/src/users/Pagination.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Pagination = ({ currentPage, goNext, goPrev, totalItems, itemsPerPage }) => {
+  const isNextDisabled = currentPage * itemsPerPage >= totalItems;
+  const isPrevDisabled = currentPage === 1;
+  return (
+    <div className="pagination">
+      <button disabled={isPrevDisabled} className="btn" onClick={goPrev}>
+        {!isPrevDisabled && '←'}
+      </button>
+      <span className="pagination__page">{currentPage}</span>
+      <button disabled={isNextDisabled} className="btn" onClick={goNext}>
+        {!isNextDisabled && '→'}
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
