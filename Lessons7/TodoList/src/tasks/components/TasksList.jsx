99fba45ad63451597155f348310e9f7d1ce597eb
taras <taraslychynets@icloud.com>
2022-02-14 13:57:42 +0200

    react component

---
+++ Lessons7/TodoList/src/tasks/components/TasksList.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Task from './Task';
+
+const TasksList = ({ tasks, handleTaskStatusChange, handleTaskDelete }) => {
+  return (
+    <ul className="list">
+      {tasks.map(task => (
+        <Task
+          key={task.id}
+          {...task}
+          onChange={handleTaskStatusChange}
+          onDelete={handleTaskDelete}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default TasksList;
